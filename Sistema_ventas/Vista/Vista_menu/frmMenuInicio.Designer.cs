3f299a13813857af562a6f8392006a4b1f21318f
Moises Antonio Quispe Cieza <quispe.moises@pucp.pe>
2017-10-20 19:55:05 -0500

    cambios en menu inicio ( solo botones )

--- Sistema_ventas/Vista/Vista_menu/frmMenuInicio.Designer.cs
+++ Sistema_ventas/Vista/Vista_menu/frmMenuInicio.Designer.cs
@@ -24,10 +24,6 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuInicio));
-            this.lblClientes = new System.Windows.Forms.Label();
-            this.lblPedidos = new System.Windows.Forms.Label();
-            this.lblProductos = new System.Windows.Forms.Label();
-            this.lblProveedores = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnPedidos = new System.Windows.Forms.Button();
             this.btnProductos = new System.Windows.Forms.Button();
@@ -35,71 +31,6 @@
             this.btnProveedores = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // lblClientes
-            // 
-            this.lblClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblClientes.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblClientes.ForeColor = System.Drawing.Color.White;
-            this.lblClientes.Location = new System.Drawing.Point(27, 89);
-            this.lblClientes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblClientes.Name = "lblClientes";
-            this.lblClientes.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.lblClientes.Size = new System.Drawing.Size(184, 44);
-            this.lblClientes.TabIndex = 0;
-            this.lblClientes.Text = "Administrar Clientes";
-            this.lblClientes.Click += new System.EventHandler(this.lblClientes_Click);
-            this.lblClientes.MouseEnter += new System.EventHandler(this.lblClientes_MouseEnter);
-            this.lblClientes.MouseLeave += new System.EventHandler(this.lblClientes_MouseLeave);
-            this.lblClientes.MouseHover += new System.EventHandler(this.lblClientes_MouseHover);
-            // 
-            // lblPedidos
-            // 
-            this.lblPedidos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblPedidos.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPedidos.ForeColor = System.Drawing.Color.White;
-            this.lblPedidos.Location = new System.Drawing.Point(27, 149);
-            this.lblPedidos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblPedidos.Name = "lblPedidos";
-            this.lblPedidos.Size = new System.Drawing.Size(184, 44);
-            this.lblPedidos.TabIndex = 1;
-            this.lblPedidos.Text = "Administrar Pedidos";
-            this.lblPedidos.Click += new System.EventHandler(this.lblPedidos_Click);
-            this.lblPedidos.MouseEnter += new System.EventHandler(this.lblPedidos_MouseEnter);
-            this.lblPedidos.MouseLeave += new System.EventHandler(this.lblPedidos_MouseLeave);
-            this.lblPedidos.MouseHover += new System.EventHandler(this.lblPedidos_MouseHover);
-            // 
-            // lblProductos
-            // 
-            this.lblProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblProductos.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProductos.ForeColor = System.Drawing.Color.White;
-            this.lblProductos.Location = new System.Drawing.Point(27, 227);
-            this.lblProductos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblProductos.Name = "lblProductos";
-            this.lblProductos.Size = new System.Drawing.Size(184, 44);
-            this.lblProductos.TabIndex = 2;
-            this.lblProductos.Text = "Administrar Productos";
-            this.lblProductos.Click += new System.EventHandler(this.lblProductos_Click_1);
-            this.lblProductos.MouseEnter += new System.EventHandler(this.lblProductos_MouseEnter);
-            this.lblProductos.MouseLeave += new System.EventHandler(this.lblProductos_MouseLeave);
-            this.lblProductos.MouseHover += new System.EventHandler(this.lblProductos_MouseHover);
-            // 
-            // lblProveedores
-            // 
-            this.lblProveedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblProveedores.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProveedores.ForeColor = System.Drawing.Color.White;
-            this.lblProveedores.Location = new System.Drawing.Point(27, 305);
-            this.lblProveedores.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblProveedores.Name = "lblProveedores";
-            this.lblProveedores.Size = new System.Drawing.Size(184, 44);
-            this.lblProveedores.TabIndex = 3;
-            this.lblProveedores.Text = "Administrar Proveedores";
-            this.lblProveedores.Click += new System.EventHandler(this.lblProveedores_Click);
-            this.lblProveedores.MouseEnter += new System.EventHandler(this.lblProveedores_MouseEnter);
-            this.lblProveedores.MouseLeave += new System.EventHandler(this.lblProveedores_MouseLeave);
-            this.lblProveedores.MouseHover += new System.EventHandler(this.lblProveedores_MouseHover);
             // 
             // panel1
             // 
@@ -109,22 +40,19 @@
             this.panel1.Controls.Add(this.btnProductos);
             this.panel1.Controls.Add(this.btnClientes);
             this.panel1.Controls.Add(this.btnProveedores);
-            this.panel1.Controls.Add(this.lblClientes);
-            this.panel1.Controls.Add(this.lblProveedores);
-            this.panel1.Controls.Add(this.lblPedidos);
-            this.panel1.Controls.Add(this.lblProductos);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(189, 531);
+            this.panel1.Size = new System.Drawing.Size(189, 412);
             this.panel1.TabIndex = 4;
             // 
             // btnPedidos
             // 
             this.btnPedidos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPedidos.BackgroundImage")));
             this.btnPedidos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPedidos.FlatAppearance.BorderSize = 0;
             this.btnPedidos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPedidos.Location = new System.Drawing.Point(12, 368);
+            this.btnPedidos.Location = new System.Drawing.Point(12, 154);
             this.btnPedidos.Name = "btnPedidos";
             this.btnPedidos.Size = new System.Drawing.Size(165, 39);
             this.btnPedidos.TabIndex = 7;
@@ -139,22 +67,27 @@
             // 
             this.btnProductos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProductos.BackgroundImage")));
             this.btnProductos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnProductos.FlatAppearance.BorderSize = 0;
             this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnProductos.Location = new System.Drawing.Point(12, 413);
+            this.btnProductos.Location = new System.Drawing.Point(12, 210);
             this.btnProductos.Name = "btnProductos";
             this.btnProductos.Size = new System.Drawing.Size(165, 39);
             this.btnProductos.TabIndex = 6;
             this.btnProductos.Text = "Productos";
             this.btnProductos.UseVisualStyleBackColor = true;
+            this.btnProductos.Click += new System.EventHandler(this.btnProductos_Click);
+            this.btnProductos.MouseEnter += new System.EventHandler(this.btnProductos_MouseEnter);
+            this.btnProductos.MouseLeave += new System.EventHandler(this.btnProductos_MouseLeave);
             // 
             // btnClientes
             // 
             this.btnClientes.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClientes.BackgroundImage")));
             this.btnClientes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClientes.FlatAppearance.BorderSize = 0;
             this.btnClientes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.btnClientes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClientes.Location = new System.Drawing.Point(12, 35);
+            this.btnClientes.Location = new System.Drawing.Point(12, 99);
             this.btnClientes.Name = "btnClientes";
             this.btnClientes.Size = new System.Drawing.Size(165, 39);
             this.btnClientes.TabIndex = 5;
@@ -168,20 +101,26 @@
             // 
             this.btnProveedores.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProveedores.BackgroundImage")));
             this.btnProveedores.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnProveedores.FlatAppearance.BorderSize = 0;
+            this.btnProveedores.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnProveedores.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btnProveedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnProveedores.Location = new System.Drawing.Point(12, 458);
+            this.btnProveedores.Location = new System.Drawing.Point(12, 266);
             this.btnProveedores.Name = "btnProveedores";
             this.btnProveedores.Size = new System.Drawing.Size(165, 39);
             this.btnProveedores.TabIndex = 4;
             this.btnProveedores.Text = "Proveedores";
             this.btnProveedores.UseVisualStyleBackColor = true;
+            this.btnProveedores.Click += new System.EventHandler(this.btnProveedores_Click);
+            this.btnProveedores.MouseEnter += new System.EventHandler(this.btnProveedores_MouseEnter);
+            this.btnProveedores.MouseLeave += new System.EventHandler(this.btnProveedores_MouseLeave);
             // 
             // frmMenuInicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(71)))), ((int)(((byte)(78)))));
-            this.ClientSize = new System.Drawing.Size(189, 531);
+            this.ClientSize = new System.Drawing.Size(189, 412);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -194,11 +133,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblClientes;
-        private System.Windows.Forms.Label lblPedidos;
-        private System.Windows.Forms.Label lblProductos;
-        private System.Windows.Forms.Label lblProveedores;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnProveedores;
         private System.Windows.Forms.Button btnPedidos;
--- Sistema_ventas/Vista/Vista_menu/frmMenuInicio.cs
+++ Sistema_ventas/Vista/Vista_menu/frmMenuInicio.cs
@@ -25,138 +25,17 @@ namespace Vista.Vista_menu
 
         }
 
-        private void lblClientes_MouseEnter(object sender, EventArgs e) {
-            //lblClientes.Font = new Font(this.Font,FontStyle.Bold | FontStyle.Italic);
-            lblClientes.Cursor = Cursors.Hand;
-        }
 
-        private void lblClientes_MouseLeave(object sender, EventArgs e) {
-            lblClientes.Text = "Administrar Clientes";
-            lblClientes.Font = new Font(this.Font,FontStyle.Regular);
-            lblClientes.Cursor = Cursors.Default;
-        }
 
-        private void lblClientes_MouseHover(object sender, EventArgs e) {
-            lblClientes.Text = "ADMINISTRAR CLIENTES";
-            lblClientes.Font = new Font(this.Font, FontStyle.Bold);
-            lblClientes.Cursor = Cursors.Hand;
-        }
 
-        private void lblPedidos_MouseHover(object sender, EventArgs e) {
-            lblPedidos.Text = "ADMINISTRAR PEDIDOS";
-            lblPedidos.Font = new Font(this.Font, FontStyle.Bold);
-            lblPedidos.Cursor = Cursors.Hand;
-        }
-
-        private void lblPedidos_MouseLeave(object sender, EventArgs e) {
-            lblPedidos.Text = "Administrar Pedidos";
-            lblPedidos.Font = new Font(this.Font, FontStyle.Regular);
-            lblPedidos.Cursor = Cursors.Default;
-        }
-
-        private void lblProductos_MouseHover(object sender, EventArgs e) {
-            lblProductos.Text = "ADMINISTRAR PRODUCTOS";
-            lblProductos.Font = new Font(this.Font, FontStyle.Bold);
-            lblProductos.Cursor = Cursors.Hand;
-        }
-
-        private void lblProductos_MouseLeave(object sender, EventArgs e) {
-            lblProductos.Text = "Administrar Productos";
-            lblProductos.Font = new Font(this.Font, FontStyle.Regular);
-            lblProductos.Cursor = Cursors.Default;
-        }
-
-        private void lblProveedores_MouseHover(object sender, EventArgs e) {
-            lblProveedores.Text = "ADMINISTRAR PROVEEDORES";
-            lblProveedores.Font = new Font(this.Font, FontStyle.Bold);
-            lblProveedores.Cursor = Cursors.Hand;
-        }
-
-        private void lblProveedores_MouseLeave(object sender, EventArgs e) {
-            lblProveedores.Text = "Administrar Proveedores";
-            lblProveedores.Font = new Font(this.Font, FontStyle.Regular);
-            lblProveedores.Cursor = Cursors.Default;
-        }
-
-        private void lblPedidos_MouseEnter(object sender, EventArgs e) {
-            //lblPedidos.Font = new Font(this.Font, FontStyle.Bold | FontStyle.Italic);
-            lblPedidos.Cursor = Cursors.Hand;
-        }
-
-        private void lblProductos_MouseEnter(object sender, EventArgs e) {
-            //lblProductos.Font = new Font(this.Font, FontStyle.Bold | FontStyle.Italic);
-            lblProductos.Cursor = Cursors.Hand;
-        }
-
-        private void lblProveedores_MouseEnter(object sender, EventArgs e) {
-            //lblProveedores.Font = new Font(this.Font, FontStyle.Bold | FontStyle.Italic);
-            lblProveedores.Cursor = Cursors.Hand;
-        }
-
-        private void lblClientes_Click(object sender, EventArgs e)
-        {
-            if (frmAdmCliente == null || frmAdmCliente.Estado == Vista.estado.Cerrado)
-            {
-                frmAdmCliente = new frmAdmCliente();
-                frmAdmCliente.MdiParent = this.MdiParent;
-                frmAdmCliente.StartPosition = FormStartPosition.Manual;
-                //frmAdmCliente.Left = 244;
-                frmAdmCliente.Left = 0;
-                frmAdmCliente.Top = 112;
-                frmAdmCliente.Show();
-                this.Dispose();
-                cabecera.Abierto = false;
-                
-                
-            }
-
-        }
 
         private void btnCerrarSesion_Click(object sender, EventArgs e)
         {
             this.Dispose();   
         }
 
-        private void lblProveedores_Click(object sender, EventArgs e)
-        {
-            if (frmAdmCliente == null || frmAdmCliente.Estado == Vista.estado.Cerrado)
-            {
-                frmAdmProv = new frmAdmProveedor();
-                frmAdmProv.MdiParent = this.MdiParent;
-           
-                frmAdmProv.StartPosition = FormStartPosition.Manual;
-                //frmAdmProv.Left = 244;
-                frmAdmProv.Left = 0;
-                frmAdmProv.Top = 112;
-                frmAdmProv.Show();
-                this.Dispose();
-                cabecera.Abierto = false;
-            }
-        }
 
-        private void lblProductos_Click_1(object sender, EventArgs e)
-        {
-            if (frmAdmProducto == null || frmAdmProducto.Estado == Vista.estado.Cerrado)
-            {
-                frmAdmProducto = new frmAdmProducto();
-                frmAdmProducto.MdiParent = this.MdiParent;
-                frmAdmProducto.StartPosition = FormStartPosition.Manual;
-                //frmAdmProducto.Left = 244;
-                frmAdmProducto.Left = 0;
-                frmAdmProducto.Top = 112;
-                frmAdmProducto.Show();
-                this.Dispose();
-                cabecera.Abierto = false;
 
-            }
-        }
-
-        private void lblPedidos_Click(object sender, EventArgs e)
-        {
-            frmAdmPedidoRegistrar form = new frmAdmPedidoRegistrar();
-            form.MdiParent = this.MdiParent;
-            form.Show();
-        }
         //boton clientes
         private void btnClientes_Click(object sender, EventArgs e) {
             if (frmAdmCliente == null || frmAdmCliente.Estado == Vista.estado.Cerrado) {
@@ -194,6 +73,51 @@ namespace Vista.Vista_menu
 
         private void btnPedidos_MouseLeave(object sender, EventArgs e) {
             this.Cursor = Cursors.Default;
+        }
+        //bton productos
+        private void btnProductos_MouseEnter(object sender, EventArgs e) {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void btnProductos_MouseLeave(object sender, EventArgs e) {
+            this.Cursor = Cursors.Default;
+        }
+        private void btnProductos_Click(object sender, EventArgs e) {
+            if (frmAdmProducto == null || frmAdmProducto.Estado == Vista.estado.Cerrado) {
+                frmAdmProducto = new frmAdmProducto();
+                frmAdmProducto.MdiParent = this.MdiParent;
+                frmAdmProducto.StartPosition = FormStartPosition.Manual;
+                //frmAdmProducto.Left = 244;
+                frmAdmProducto.Left = 0;
+                frmAdmProducto.Top = 112;
+                frmAdmProducto.Show();
+                this.Dispose();
+                cabecera.Abierto = false;
+
+            }
+        }
+        //boton proveedores
+        private void btnProveedores_MouseEnter(object sender, EventArgs e) {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void btnProveedores_MouseLeave(object sender, EventArgs e) {
+            this.Cursor = Cursors.Default;
+        }
+
+        private void btnProveedores_Click(object sender, EventArgs e) {
+            if (frmAdmCliente == null || frmAdmCliente.Estado == Vista.estado.Cerrado) {
+                frmAdmProv = new frmAdmProveedor();
+                frmAdmProv.MdiParent = this.MdiParent;
+
+                frmAdmProv.StartPosition = FormStartPosition.Manual;
+                //frmAdmProv.Left = 244;
+                frmAdmProv.Left = 0;
+                frmAdmProv.Top = 112;
+                frmAdmProv.Show();
+                this.Dispose();
+                cabecera.Abierto = false;
+            }
         }
     }
 }
