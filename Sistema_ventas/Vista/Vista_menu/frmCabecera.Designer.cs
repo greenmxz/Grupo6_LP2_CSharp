e60b346d1d21bb28cc1f385ba79ad8e2eeed7c77
Jean Piere Sullon <Piero_14-02@hotmail.com>
2017-11-22 20:20:03 -0500

    Modificación de ingresos con Usuario sistemas sistemas

--- Sistema_ventas/Vista/Vista_menu/frmCabecera.Designer.cs
+++ Sistema_ventas/Vista/Vista_menu/frmCabecera.Designer.cs
@@ -75,6 +75,7 @@
             this.btnMenu.Size = new System.Drawing.Size(40, 34);
             this.btnMenu.TabIndex = 3;
             this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
             this.btnMenu.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnMenu_MouseClick);
             this.btnMenu.MouseEnter += new System.EventHandler(this.btnMenu_MouseEnter);
             this.btnMenu.MouseLeave += new System.EventHandler(this.btnMenu_MouseLeave);
